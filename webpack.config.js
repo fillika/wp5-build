02cd22424cc4822f46cd5fb2c09e6140b8654ab8
amironenko <alfie.mir@gmail.com>
2022-02-27 14:31:51 +0300

    Добавлен postCSS + sass loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const miniCss = require('mini-css-extract-plugin');
 
 let mode = process.env.NODE_ENV === 'production' ? 'production' : 'development';
 
@@ -29,11 +30,18 @@ module.exports = {
                 use: 'ts-loader',
                 exclude: /node_modules/,
             },
+            {
+                test: /\.((s[ac]|s)ss)|(.css)$/i,
+                use: [miniCss.loader, 'css-loader', 'sass-loader', 'postcss-loader'],
+            },
         ],
     },
     plugins: [
         new HtmlWebpackPlugin({
             template: path.resolve(__dirname, 'src', 'index.html'),
+        }),
+        new miniCss({
+            filename: 'style.css',
         }),
     ],
 };
