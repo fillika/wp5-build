698833a653c8543fbf240d8748bb83f0fa4bed24
amironenko <alfie.mir@gmail.com>
2022-02-27 19:12:31 +0300

    Добавлена поддержка styled component

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,5 +1,13 @@
 import * as React from 'react';
+import styled from 'styled-components';
+
+const Title = styled.h1`
+    color: saddlebrown;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
 
 export const App: React.FC = () => {
-    return <div>Start here...</div>;
+    return <Title>Start here...</Title>;
 };
